f37c1e52e12a8ab89a9ca88fd1c8b2145e558d84
hayoung <home0@naver.com>
2022-12-14 16:29:18 +0900

    해쉬라우터로 변경

--- dayjs-todo/react/src/index.tsx
+++ dayjs-todo/react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { persistStore } from "redux-persist";
@@ -12,12 +12,12 @@ const persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
         <App />
         </Provider>
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
